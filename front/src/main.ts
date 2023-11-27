b878b63d1d82c25d0840f6318fb2ec68721d75c6
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-11-27 14:30:52 +0100

    dev: converted to typescript

--- front/src/main.ts
+++ front/src/main.ts
@@ -10,7 +10,7 @@ const multiplicationFactor = 2;
 
 const r = 1;
 
-const getSamplePosition = (i, samples) => {
+const getSamplePosition = (i: number, samples: number) => {
   const angle = (i * 2 * Math.PI) / samples - Math.PI / 2;
 
   const x = cx0 + r0 * Math.cos(angle);
@@ -19,6 +19,9 @@ const getSamplePosition = (i, samples) => {
 };
 
 const container = document.querySelector("svg .samples");
+if (container === null) {
+  throw new Error("oups");
+}
 console.log("container: ", container);
 for (let i = 0; i < samples; i++) {
   const elt = document.createElementNS(svgns, "circle");
@@ -26,14 +29,17 @@ for (let i = 0; i < samples; i++) {
 
   const { x: cx, y: cy } = getSamplePosition(i, samples);
 
-  elt.setAttributeNS(null, "cx", cx);
-  elt.setAttributeNS(null, "cy", cy);
-  elt.setAttributeNS(null, "r", r);
+  elt.setAttributeNS(null, "cx", cx + "");
+  elt.setAttributeNS(null, "cy", cy.toString());
+  elt.setAttributeNS(null, "r", String(r));
 
   container.appendChild(elt);
 }
 
 const lineContainer = document.querySelector("svg .lines");
+if (lineContainer === null) {
+  throw new Error("oups");
+}
 console.log("lineContainer: ", lineContainer);
 for (let i = 0; i < samples; i++) {
   const elt = document.createElementNS(svgns, "line");
@@ -42,10 +48,10 @@ for (let i = 0; i < samples; i++) {
   const p1 = getSamplePosition(i, samples);
   const p2 = getSamplePosition(i * multiplicationFactor, samples);
 
-  elt.setAttributeNS(null, "x1", p1.x);
-  elt.setAttributeNS(null, "x2", p2.x);
-  elt.setAttributeNS(null, "y1", p1.y);
-  elt.setAttributeNS(null, "y2", p2.y);
+  elt.setAttributeNS(null, "x1", p1.x + "");
+  elt.setAttributeNS(null, "x2", p2.x + "");
+  elt.setAttributeNS(null, "y1", p1.y + "");
+  elt.setAttributeNS(null, "y2", p2.y + "");
 
   lineContainer.appendChild(elt);
 }
